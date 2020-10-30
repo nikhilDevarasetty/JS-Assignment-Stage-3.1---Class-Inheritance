ea4e9a277045851e94911a19e6e4ee52ac64084e
nikhilDevarasetty <71460914+nikhilDevarasetty@users.noreply.github.com>
2020-10-30 23:11:50 +0530

    Update shape.js
    created shape function

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,19 @@
 // declare class
 
 // export class using module.exports
+
+module.exports=class Shape
+{
+constructor()
+{
+this.color="red";
+}
+drawShape()
+{
+console.log("Drawing");
+ }
+calculateArea()
+{
+console.log("calculating");
+ }
+}
