c48037da196712ef40a9a5d643eb8981f52e026f
nikhilDevarasetty <71460914+nikhilDevarasetty@users.noreply.github.com>
2020-10-30 23:10:49 +0530

    Update circle.js
    created circle function

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,16 @@
 
 
 // export class using module.exports
+let  Shape=require('./shape');
+module.exports=class Circle extends Shape
+{
+constructor()
+{
+super()
+}
+ calculateArea()
+ {
+console.log("Area of circle with color: "+this.color);
+ return 3.14*5*5;
+ }
+}
